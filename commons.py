fbdc1db8a01b1d1bb5bc7765b4b485aab011612a
rmzneydev <neider9725@gmail.com>
2026-03-20 08:31:11 -0500

    some visual changes in show_state funtion

--- commons.py
+++ commons.py
@@ -3,22 +3,26 @@ def generate_damage(min:int, max:int):
     damage = random.randint(min, max)
     return damage
 
-def show_state(hero_name: str, hero_hp:int, enemy_name:str, enemy_hp:int):
+def show_state(hero_name: str, hero_hp:int, enemy_name:str, enemy_hp:int, potions:int):
     max_hero_hp = 100
     max_enemy_hp = 120
     
     lost_hp = max_hero_hp - hero_hp
-    print("#"*10, " GAME STATUS ","#"*11)
-    print("-"*15, "HERO", "-"*15 )
-    print(F"NAME: {hero_name}")
-    print(F"HP: {hero_hp}")
-    print(F"LIFE BAR [{"#"*hero_hp}{"-"*lost_hp}]")
-          
+    print("╔"+"═"*60, "GAME STATUS","═"*60+"╗")
+    print(" > HERO")
+    print(f"   NAME: {hero_name}")
+    print(f"   HP: {hero_hp}/{max_hero_hp}")
+    print(f"   Potions: {potions}")
+    print(f"   LIFE [{"#"*hero_hp}{"-"*lost_hp}]")
+    print("═"*135)
     lost_hp = max_enemy_hp - enemy_hp
-    print("-"*15, "ENEMY", "-"*14)
-    print(F"NAME: {enemy_name}")
-    print(F"HP: {enemy_hp}")
-    print(F"LIFE BAR [{"#"*enemy_hp}{"-"*lost_hp}]")
+    print(" > ENEMY")
+    print(f"   NAME: {enemy_name}")
+    print(f"   HP: {enemy_hp}/{max_enemy_hp}")
+    print(f"   LIFE [{"#"*enemy_hp}{"-"*lost_hp}]")
+    print("╚"+"═"*134+"╝")
+          
+
 
 def verify_winner(hero_hp:int, enemy_hp:int):
     if hero_hp <= 0 or enemy_hp <= 0:
